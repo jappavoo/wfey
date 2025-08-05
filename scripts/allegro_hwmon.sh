7fe9083906e3320494a6d06356aa25ce6c593229
CJ Parra <acparra@bu.edu>
2025-08-05 19:31:56 +0000

    feat(runner scripts): portable energy gathering in the runner scripts

---
+++ scripts/allegro_hwmon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# --------- Run HWMON --------- #
+
+function powerLog() {
+    LOGPATH=$1
+    LOGDATE=$2
+    echo "hwmon2 hwmon3" > $LOGPATH/hwmon-${LOGDATE}.out
+    while sleep 0.5; do
+	echo $(cat /sys/class/hwmon/hwmon2/power1_input /sys/class/hwmon/hwmon3/power1_input) >> $LOGPATH/hwmon-${LOGDATE}.out
+    done
+}
--- scripts/run_wfey.sh
+++ scripts/run_wfey.sh
@@ -36,16 +36,13 @@ fi
 mkdir -p $LOGPATH
 
 
-# --------- Run HWMON --------- #
+# --------- Run ENERGY SCRIPT --------- #
+## This script should contain a function called 'powerLog' which should
+## take in the Path and the Data args to create a file '$LOGPATH/hwmon-${LOGDATE}.out'
 
-powerLog() {
-    echo "hwmon2 hwmon3" > $LOGPATH/hwmon-${LOGDATE}.out
-    while sleep 0.5; do
-	echo $(cat /sys/class/hwmon/hwmon2/power1_input /sys/class/hwmon/hwmon3/power1_input) >> $LOGPATH/hwmon-${LOGDATE}.out
-    done
-}
+source $ENERGYSCRIPT
 
-powerLog &
+powerLog "$LOGPATH" "$LOGDATE" &
 powerPID=$!
 
 # ---------- Run WFEY ---------- #
@@ -55,21 +52,3 @@ powerPID=$!
 sleep ${SLEEP_TO_FINISH} # Power numbers continue to go up a little after the wfey code is done
 
 kill -15 $powerPID
-
-
-
-#$SCRIPTDIR/minmax_cpu_power.sh 2 1 &> $LOGPATH/hwmon2-${LOGDATE}.out &
-#$SCRIPTDIR/minmax_cpu_power.sh 3 1 &> $LOGPATH/hwmon3-${LOGDATE}.out &
-
-#$SCRIPTDIR/minmax_kill.sh
-
-# Get Min
-# tail -n hwmon<n>*.out | cut -d ':' -f 2 | cut -d ' ' -f 1
-# cat hwmon-*.out | cut -f 1 -d ' ' | tail -n +2 | sort | head -1
-
-# Get Max
-# tail -n hwmon<n>*.out | cut -d ':' -f 3
-# # cat hwmon-*.out | cut -f 1 -d ' ' | tail -n +2 | sort -r | head -1
-
-# Get Diff
-# echo $max-$min | bc
